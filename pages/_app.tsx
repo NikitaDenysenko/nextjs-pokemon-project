5d1463200aa4d2608796921a46703c69c12f7505
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2022-08-15 20:26:06 +0300

    feat: setup mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import { FC } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import '../styles/globals.css'
+import lightThemeOptions from '../styles/theme/lightThemeOptions'
+import createEmotionCache from '../utility/createEmotionCache'
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+const lightTheme = createTheme(lightThemeOptions)
+
+const MyApp: FC<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
